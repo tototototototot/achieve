3f65ed3e85fdafbacab3be323009c4c07d7449b2
teruyasu takahashi <taka-t@nttpc.co.jp>
2017-08-08 13:11:31 +0000

    20170808

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  name = Faker::Internet.user_name
-  User.create!(email: email,
-               name: name,
-               password: password,
-               password_confirmation: password,
-               )
+#100.times do |n|
+#  email = Faker::Internet.email
+#  password = "password"
+#  name = Faker::Internet.user_name
+#  User.create!(email: email,
+#               name: name,
+#               password: password,
+#               password_confirmation: password,
+#               )
+#end
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n
+  )
+  n = n + 1
 end
